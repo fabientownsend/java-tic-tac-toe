d7aa7a363d5f675ac1d0b6f15d6f6aa6f91e6ff8
fabien townsend <fabientownsend@gmail.com>
2016-11-17 10:04:03 +0000

    main for the game

--- src/main/java/tictactoe/Main.java
+++ src/main/java/tictactoe/Main.java
@@ -1,7 +1,17 @@
 package tictactoe;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+
 public class Main {
     public static void main(String[] args){
-      System.out.println("Hello");
+        BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+        PrintWriter output = new PrintWriter(System.out, true);
+        IOGame io = new IO(input, output);
+
+        GamePlay gp = new GamePlay(io);
+        gp.play();
+        System.out.println("The party is over");
     }
 }
