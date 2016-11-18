7b7fcdf3da8c09781f7d495a040c073e85c2f175
fabien townsend <fabientownsend@gmail.com>
2016-11-18 17:24:35 +0000

    add test exception handle io

--- src/test/java/tictactoe/CommandLineTest.java
+++ src/test/java/tictactoe/CommandLineTest.java
@@ -3,10 +3,7 @@ package tictactoe;
 import org.junit.Before;
 import org.junit.Test;
 
-import java.io.BufferedReader;
-import java.io.PrintWriter;
-import java.io.StringReader;
-import java.io.StringWriter;
+import java.io.*;
 
 import static org.junit.Assert.*;
 
@@ -33,4 +30,26 @@ public class CommandLineTest {
     public void itReadFromTheCommandLine() throws Exception {
         assertEquals(cli.read(), "world");
     }
+
+    @Test
+    public void itRaiseAnException() throws Exception {
+        BufferedReader input = new FakeBuffered(new StringReader("world"));
+        out = new StringWriter();
+        PrintWriter output = new PrintWriter(out, true);
+        cli = new CommandLine(input, output);
+
+        assertEquals(cli.read(), "Error");
+    }
 }
+
+final class FakeBuffered extends BufferedReader {
+    FakeBuffered(final Reader reader) {
+        super(reader);
+    }
+
+    @Override
+    public String readLine() throws IOException {
+        throw new IOException();
+    }
+}
+
