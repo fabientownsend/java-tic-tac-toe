0845069e227c372f890c276e85e8eba2343475e2
fabien townsend <fabientownsend@gmail.com>
2016-11-28 12:42:45 +0000

    the board converter can display bigger board proprelly

--- src/main/java/tictactoe/BoardConverter.java
+++ src/main/java/tictactoe/BoardConverter.java
@@ -16,7 +16,10 @@ public class BoardConverter {
             }
 
             if (++idLine != board.length) {
-                str.append("-----------\n");
+                for (int i = 0; i < board.length * board.length + 1 * board.length - 1; i++) {
+                    str.append("-");
+                }
+                str.append("\n");
             }
         }
 
@@ -24,25 +27,44 @@ public class BoardConverter {
     }
 
     private String createLine(int idSpot, Marks mark) {
-        return " " + markToDisplay(mark, idSpot) + rightEdgeSpot(idSpot);
+        return markToDisplay(mark, idSpot) + rightEdgeSpot(idSpot);
     }
 
     private String rightEdgeSpot(int y) {
         if (isBoardEdge(y)) {
-            return " \n";
+            return "\n";
         } else {
-            return " |";
+            return "|";
         }
     }
 
-    private char markToDisplay(Marks mark, int idSpot) {
+    private String markToDisplay(Marks mark, int idSpot) {
+        String result = "";
         if (mark == Marks.CROSS) {
-            return 'X';
+            result += "X";
         } else if (mark == Marks.ROUND) {
-            return 'O';
+            result += "O";
         } else {
-            return Character.forDigit(idSpot, 10);
+            result += String.valueOf(idSpot);
         }
+        return center(result, board.length);
+    }
+
+    public String center(String str, int width) {
+        int paddenWidth = (width - str.length()) / 2;
+        if (str.length() % 2 == 0 || width % 2 != 0) {
+            return padden(paddenWidth) + str + padden(paddenWidth);
+        } else {
+            return padden(paddenWidth) + " " + str + padden(paddenWidth);
+        }
+    }
+
+    private String padden(int width) {
+        StringBuilder padden = new StringBuilder();
+        for (int i = 0; i < width; i++) {
+            padden.append(" ");
+        }
+        return padden.toString();
     }
 
     private boolean isBoardEdge(int i) {
--- src/test/java/tictactoe/BoardConverterTest.java
+++ src/test/java/tictactoe/BoardConverterTest.java
@@ -1,12 +1,13 @@
 package tictactoe;
 
+import org.junit.Ignore;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class BoardConverterTest {
     @Test
-    public void stringiphyAnArrayToString() throws Exception {
+    public void stringiphyAThreeByThreeBoard() {
         BoardConverter boardConverter = new BoardConverter();
         Marks[][] board = new Marks[3][3];
         String boardString = " 0 | 1 | 2 \n"
@@ -19,7 +20,7 @@ public class BoardConverterTest {
     }
 
     @Test
-    public void boardStringWithAMark() throws Exception {
+    public void boardStringWithAMark() {
         BoardConverter boardConverter = new BoardConverter();
         Marks[][] board = new Marks[3][3];
         board[1][1] = Marks.CROSS;
@@ -31,5 +32,44 @@ public class BoardConverterTest {
                            + " 6 | 7 | 8 \n";
 
         assertEquals(boardString, boardConverter.toString(board));
+    }
+
+    @Test
+    public void stringiphyAFourByFourBoard() {
+        BoardConverter boardConverter = new BoardConverter();
+        Marks[][] board = new Marks[4][4];
+        String boardString = "  0 |  1 |  2 |  3 \n"
+                           + "-------------------\n"
+                           + "  4 |  5 |  6 |  7 \n"
+                           + "-------------------\n"
+                           + "  8 |  9 | 10 | 11 \n"
+                           + "-------------------\n"
+                           + " 12 | 13 | 14 | 15 \n";
+
+        assertEquals(boardString, boardConverter.toString(board));
+    }
+
+    @Test
+    public void methodThatCenterAString() {
+        BoardConverter boardConverter = new BoardConverter();
+        assertEquals(boardConverter.center("test", 6), " test ");
+    }
+
+    @Test
+    public void methodThatCenterAStringTwo() {
+        BoardConverter boardConverter = new BoardConverter();
+        assertEquals("  test  ", boardConverter.center("test", 8));
+    }
+
+    @Test
+    public void handleTheCaseOfAnEvenString() {
+        BoardConverter boardConverter = new BoardConverter();
+        assertEquals("   est  ", boardConverter.center("est", 8));
+    }
+
+    @Test
+    public void expectEvenWidth() {
+        BoardConverter boardConverter = new BoardConverter();
+        assertEquals(" 1 ", boardConverter.center("1", 3));
     }
 }
