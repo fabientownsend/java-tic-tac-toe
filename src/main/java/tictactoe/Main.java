2f288b0c9622ad8c4037c3217c464a9bae2d92f7
fabien townsend <fabientownsend@gmail.com>
2016-11-30 17:53:45 +0000

    update main with the menu

--- src/main/java/tictactoe/Main.java
+++ src/main/java/tictactoe/Main.java
@@ -9,12 +9,27 @@ public class Main {
         BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
         PrintWriter output = new PrintWriter(System.out, true);
         IO io = new CommandLine(input, output);
-        Board board = new Board(4);
+        Menu menu = new Menu(io);
+        int boardSize = menu.sizeBoard(3, 5);
+        int gameType = menu.typeGame(1, 3);
 
-        Player playerOne = new CommandLinePlayer(io, Marks.CROSS);
-        Player playerTwo = new ComputerPlayer(Marks.ROUND, board);
+        Board board = new Board(boardSize);
+        PlayerFactory playerFactory = new PlayerFactory(io, board);
+        Player[] players = playerFactory.getPlayers(convert(gameType));
 
-        GamePlay game = new GamePlay(io, board, playerOne, playerTwo);
+        GamePlay game = new GamePlay(io, board, players[0], players[1]);
         game.play();
+    }
+
+    private static String convert(int gameType) {
+        if (gameType == 1) {
+            return "HUMANvsHUMAN";
+        } else if (gameType == 2) {
+            return "COMPUTERvsHUMAN";
+        } else if (gameType == 3) {
+            return "COMPUTERvsCOMPUTER";
+        } else {
+            return "HUMANvsHUMAN";
+        }
     }
 }
