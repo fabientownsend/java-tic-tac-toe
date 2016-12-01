1a38608df4cd81fc6d53c8891f47b926d5876b46
fabien townsend <fabientownsend@gmail.com>
2016-12-01 14:59:53 +0000

    improve menu test coverage

--- src/test/java/tictactoe/MenuTest.java
+++ src/test/java/tictactoe/MenuTest.java
@@ -13,66 +13,79 @@ import static org.junit.Assert.assertTrue;
 public class MenuTest {
     private StringWriter out;
     private IO fakeCommandLine;
+    private Menu menu;
 
     @Test
     public void displayMessageForBoardSize() {
-        initialisationFakeIO("3\n");
-        Menu menu = new Menu(fakeCommandLine);
+        initialisationMenuInput("3\n");
         menu.sizeBoard();
         assertTrue(out.toString().contains("Select board size: "));
     }
 
     @Test
     public void returnMinValueAsDefaultValueWhenInputEmpty() {
-        initialisationFakeIO("\n");
-        Menu menu = new Menu(fakeCommandLine);
+        initialisationMenuInput("\n");
         assertEquals(menu.sizeBoard(), 3);
     }
 
     @Test
     public void displayMessageForTypeGame() {
-        initialisationFakeIO("1\n");
-        Menu menu = new Menu(fakeCommandLine);
+        initialisationMenuInput("1\n");
         menu.typeGame();
         assertTrue(out.toString().contains("What kind of game do you want to play?"));
     }
 
     @Test
+    public void menuReturnHumanVsHumanGameType() {
+        initialisationMenuInput("1\n");
+        assertEquals(menu.typeGame(), GameTypes.HUMAN_VS_HUMAN);
+    }
+
+    @Test
+    public void menuReturnHumanVsComputerGameType() {
+        initialisationMenuInput("2\n");
+        assertEquals(menu.typeGame(), GameTypes.HUMAN_VS_COMPUTER);
+    }
+
+    @Test
+    public void menuReturnComputerVsComputerGameType() {
+        initialisationMenuInput("3\n");
+        assertEquals(menu.typeGame(), GameTypes.COMPUTER_VS_COMPUTER);
+    }
+
+    @Test
     public void returnTheUserInput() {
-        initialisationFakeIO("3\n");
-        Menu menu = new Menu(fakeCommandLine);
+        initialisationMenuInput("3\n");
         assertEquals(menu.sizeBoard(), 3);
     }
 
     @Test
     public void askUntilToGetIntegerValue() {
-        initialisationFakeIO("dsajfl;\n3\n");
-        Menu menu = new Menu(fakeCommandLine);
+        initialisationMenuInput("text\n3\n");
         assertEquals(menu.sizeBoard(), 3);
         assertTrue(out.toString().contains("The value must be an integer"));
     }
 
     @Test
     public void askAgainWhenSizeTooBig() {
-        initialisationFakeIO("55\n3\n");
-        Menu menu = new Menu(fakeCommandLine);
+        initialisationMenuInput("55\n3\n");
         menu.sizeBoard();
         assertTrue(out.toString().contains("Select value between: 3 and 5"));
     }
 
     @Test
     public void askAgainWhenSizeTooSmall() {
-        initialisationFakeIO("-1234\n3\n");
-        Menu menu = new Menu(fakeCommandLine);
+        initialisationMenuInput("-1234\n3\n");
         menu.sizeBoard();
         assertTrue(out.toString().contains("Select value between: 3 and 5"));
     }
 
-    private void initialisationFakeIO(String text) {
+    private void initialisationMenuInput(String text) {
         BufferedReader input = new BufferedReader(new StringReader(text));
         out = new StringWriter();
         PrintWriter output = new PrintWriter(out, true);
 
         fakeCommandLine = new FakeIO(input, output);
+        menu = new Menu(fakeCommandLine);
     }
 }
