d5c8cbccb95c7b6fe05381f427824389e7f892d5
fabien townsend <fabientownsend@gmail.com>
2016-12-02 11:42:16 +0000

    use assertj for its tests

--- src/test/java/tictactoe/PlayerFactoryTest.java
+++ src/test/java/tictactoe/PlayerFactoryTest.java
@@ -1,38 +1,39 @@
 package tictactoe;
 
 import org.junit.Test;
-
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class PlayerFactoryTest {
     @Test
     public void itCreateTwoCommandLinePlayers() {
         PlayerFactory playerFactory = new PlayerFactory(null, null);
         Player[] players = playerFactory.getPlayers(GameTypes.HUMAN_VS_HUMAN);
-        assertTrue(players[0] instanceof CommandLinePlayer);
-        assertEquals(players[0].getMark(), Marks.CROSS);
-        assertTrue(players[1] instanceof CommandLinePlayer);
-        assertEquals(players[1].getMark(), Marks.ROUND);
+
+        assertThat(players[0]).isInstanceOf(CommandLinePlayer.class);
+        assertThat(players[0].getMark()).isEqualTo(MarksEnum.CROSS);
+        assertThat(players[1]).isInstanceOf(CommandLinePlayer.class);
+        assertThat(players[1].getMark()).isEqualTo(MarksEnum.ROUND);
     }
 
     @Test
     public void itCreateAComputerPlayerAndCommandLinePlayer() {
         PlayerFactory playerFactory = new PlayerFactory(null, null);
         Player[] players = playerFactory.getPlayers(GameTypes.HUMAN_VS_COMPUTER);
-        assertTrue(players[0] instanceof CommandLinePlayer);
-        assertEquals(players[0].getMark(), Marks.CROSS);
-        assertTrue(players[1] instanceof ComputerPlayer);
-        assertEquals(players[1].getMark(), Marks.ROUND);
+
+        assertThat(players[0]).isInstanceOf(CommandLinePlayer.class);
+        assertThat(players[0].getMark()).isEqualTo(MarksEnum.CROSS);
+        assertThat(players[1]).isInstanceOf(ComputerPlayer.class);
+        assertThat(players[1].getMark()).isEqualTo(MarksEnum.ROUND);
     }
 
     @Test
     public void itCreateAComputerPlayerAndComputerPlayer() {
         PlayerFactory playerFactory = new PlayerFactory(null, null);
         Player[] players = playerFactory.getPlayers(GameTypes.COMPUTER_VS_COMPUTER);
-        assertTrue(players[0] instanceof ComputerPlayer);
-        assertEquals(players[0].getMark(), Marks.CROSS);
-        assertTrue(players[1] instanceof ComputerPlayer);
-        assertEquals(players[1].getMark(), Marks.ROUND);
+
+        assertThat(players[0]).isInstanceOf(ComputerPlayer.class);
+        assertThat(players[0].getMark()).isEqualTo(MarksEnum.CROSS);
+        assertThat(players[1]).isInstanceOf(ComputerPlayer.class);
+        assertThat(players[1].getMark()).isEqualTo(MarksEnum.ROUND);
     }
 }
